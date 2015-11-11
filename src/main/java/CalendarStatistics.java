cc857411d1ba02dadab08fb1089acfd1a9436ccd
Tiffany Qi <tiffany19qi@berkeley.edu>
2015-11-11 14:26:59 -0800

    iterate through events on different calendars

--- src/main/java/CalendarStatistics.java
+++ src/main/java/CalendarStatistics.java
@@ -18,111 +18,118 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
 
 public class CalendarStatistics {
-    /** Application name. */
-    private static final String APPLICATION_NAME =
-        "Summary & Statistics of Google Calendars";
+	/** Application name. */
+	private static final String APPLICATION_NAME =
+		"Summary of Hours Spent From Your Calendar";
 
-    /** Directory to store user credentials for this application. */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-        System.getProperty("user.home"), ".credentials/calendar-java-statistics");
+	/** Directory to store user credentials for this application. */
+	private static final java.io.File DATA_STORE_DIR = new java.io.File(
+		System.getProperty("user.home"), ".credentials/calendar-java-statistics");
 
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
+	/** Global instance of the {@link FileDataStoreFactory}. */
+	private static FileDataStoreFactory DATA_STORE_FACTORY;
 
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-        JacksonFactory.getDefaultInstance();
+	/** Global instance of the JSON factory. */
+	private static final JsonFactory JSON_FACTORY =
+		JacksonFactory.getDefaultInstance();
 
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
+	/** Global instance of the HTTP transport. */
+	private static HttpTransport HTTP_TRANSPORT;
 
-    /** Global instance of the scopes required by this quickstart. */
-    private static final List<String> SCOPES =
-        Arrays.asList(CalendarScopes.CALENDAR_READONLY);
+	/** Global instance of the scopes required by this quickstart. */
+	private static final List<String> SCOPES =
+		Arrays.asList(CalendarScopes.CALENDAR_READONLY);
 
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
-        }
-    }
+	static {
+		try {
+			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    /**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize() throws IOException {
-        // Load client secrets.
-        InputStream in =
-            CalendarStatistics.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+	/**
+	 * Creates an authorized Credential object.
+	 * @return an authorized Credential object.
+	 * @throws IOException
+	 */
+	public static Credential authorize() throws IOException {
+		// Load client secrets.
+		InputStream in =
+			CalendarStatistics.class.getResourceAsStream("/client_secret.json");
+		GoogleClientSecrets clientSecrets =
+			GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
-                .setAccessType("offline")
-                .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-            flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;
-    }
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow =
+				new GoogleAuthorizationCodeFlow.Builder(
+						HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+				.setDataStoreFactory(DATA_STORE_FACTORY)
+				.setAccessType("offline")
+				.build();
+		Credential credential = new AuthorizationCodeInstalledApp(
+			flow, new LocalServerReceiver()).authorize("user");
+		System.out.println(
+				"Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+		return credential;
+	}
 
-    /**
-     * Build and return an authorized Calendar client service.
-     * @return an authorized Calendar client service
-     * @throws IOException
-     */
-    public static com.google.api.services.calendar.Calendar
-        getCalendarService() throws IOException {
-        Credential credential = authorize();
-        return new com.google.api.services.calendar.Calendar.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
+	/**
+	 * Build and return an authorized Calendar client service.
+	 * @return an authorized Calendar client service
+	 * @throws IOException
+	 */
+	public static com.google.api.services.calendar.Calendar
+		getCalendarService() throws IOException {
+		Credential credential = authorize();
+		return new com.google.api.services.calendar.Calendar.Builder(
+				HTTP_TRANSPORT, JSON_FACTORY, credential)
+				.setApplicationName(APPLICATION_NAME)
+				.build();
+	}
 
-    public static void main(String[] args) throws IOException {
-        // Build a new authorized API client service.
-        // Note: Do not confuse this class with the
-        //   com.google.api.services.calendar.model.Calendar class.
-        com.google.api.services.calendar.Calendar service =
-            getCalendarService();
+	public static void main(String[] args) throws IOException {
+		// Build a new authorized API client service.
+		// Note: Do not confuse this class with the
+		//   com.google.api.services.calendar.model.Calendar class.
+		com.google.api.services.calendar.Calendar service =
+			getCalendarService();
+		HashMap<String, List<Event>> calendarToHour = new HashMap<>();
+		DateTime now = new DateTime(System.currentTimeMillis());
 
-        // List the next 10 events from the primary calendar.
-        System.out.println("----Starting Summary!-----");
+		// Summarize the hours within each calendar
+		System.out.println("----Starting Summary!-----");
 
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("primary")
-            .setMaxResults(10)
-            .setTimeMin(now)
-            .setOrderBy("startTime")
-            .setSingleEvents(true)
-            .execute();
+		String pageToken = null;
+		do {
+			CalendarList calendarList = service.calendarList().list().setPageToken(pageToken).execute();
+			List<CalendarListEntry> items = calendarList.getItems();
 
-        List<Event> items = events.getItems();
-        if (items.size() == 0) {
-            System.out.println("No upcoming events found.");
-        } else {
-            System.out.println("Upcoming events");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                if (start == null) {
-                    start = event.getStart().getDate();
-                }
-                System.out.printf("%s (%s)\n", event.getSummary(), start);
-            }
-        }
-    }
+			// iterates through all the calendars
+			for (CalendarListEntry calendarListEntry : items) {
+				if (!calendarListEntry.isPrimary()) {
+					Events events = service.events().list(calendarListEntry.getId())
+						.setMaxResults(3)
+						.setTimeMin(now)
+						.setOrderBy("startTime")
+						.setSingleEvents(true)
+						.execute();
+					List<Event> calendarItems = events.getItems();
+					long count = 0;
+					for (Event e : calendarItems) {
+						// long duration = e.getEnd().getValue() - e.getStart().getValue();
+						// System.out.println(e.getStart().getDateTime());
+					}
+					calendarToHour.put(calendarListEntry.getSummary(), calendarItems);
+				}
+			}
+			pageToken = calendarList.getNextPageToken();
+		} while (pageToken != null);
+	}
 
 }
