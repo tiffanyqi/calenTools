78f308007e024847e66d822580679c128933c246
Tiffany Qi <tiffany19qi@berkeley.edu>
2016-05-16 00:19:13 -0700

    some renaming and moving around

--- src/main/java/CalenTools.java
+++ src/main/java/CalenTools.java
@@ -16,96 +16,201 @@ import com.google.api.services.calendar.model.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
+
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.TreeMap;
+
 
 public class CalenTools {
-	/** Application name. */
-	private static final String APPLICATION_NAME =
-		"Summary of Hours Spent From Your Calendar";
 
-	/** Directory to store user credentials for this application. */
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(
-		System.getProperty("user.home"), ".credentials/calendar-java-statistics");
+    // change these variables to modify program
+    private String dateStart = "5/15/2016";
+    private String dateEnd = "5/22/2016";
+    private String productiveAreas[] = 
+        new String[]{"Studying & Learning", "Job Searching", "Work", "Planning"};
+    private String[] categoryTitle = new String[]{"Projects", "Language"};
+    private String[][] categoryFilters = 
+        new String[][]{{"productivity", "calentools", "job manager"}, {"language", "korean", "japanese"}};
+    
+    // Instantiates a new class, CalenTools
+    public CalenTools() { }
 
-	/** Global instance of the {@link FileDataStoreFactory}. */
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
+    public static void main(String[] args) throws IOException {
+        // Build a new authorized API client service. Not the calendar.model.Calendar class.
+        com.google.api.services.calendar.Calendar service =
+            getCalendarService();
 
-	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY =
-		JacksonFactory.getDefaultInstance();
+        // sets up the application
+        CalenTools c = new CalenTools();
+        HashMap<String, Double> calendarToHour = new HashMap<>();
+        HashMap<String, Double> categoryToHour = new HashMap<>();
+        HashMap<String, HashSet<String>> miscCategories; // a mapping from category name to what it contains
+        
+        DateTime now = new DateTime(System.currentTimeMillis());
+        long startTime = now.getValue();
+        int eventCount = 0;
+        miscCategories = c.addCategories(c.categoryTitle, c.categoryFilters);
 
-	/** Global instance of the HTTP transport. */
-	private static HttpTransport HTTP_TRANSPORT;
+        String pageToken = null;
+        do {
+            CalendarList calendarList = service.calendarList().list().setPageToken(pageToken).execute();
+            List<CalendarListEntry> items = calendarList.getItems();
 
-	/** Global instance of the scopes required by this quickstart. */
-	private static final List<String> SCOPES =
-		Arrays.asList(CalendarScopes.CALENDAR_READONLY);
+            // adds miscellaneous categories to the calendar map
+            for (String cat : miscCategories.keySet()) {
+                categoryToHour.put(cat, 0.0);
+            }
+            
+            // iterates through all the calendars
+            for (CalendarListEntry calendarListEntry : items) {
+                if (!calendarListEntry.isPrimary()) {
+                    
+                    // sets a list of events from a particular calendar
+                    Events events = service.events().list(calendarListEntry.getId())
+                        .setTimeMin(c.setDate(c.dateStart))
+                        .setTimeMax(c.setDate(c.dateEnd))
+                        .setOrderBy("startTime")
+                        .setSingleEvents(true)
+                        .execute();
+                    List<Event> calendarItems = events.getItems();
+                    
+                    double calendarTime = 0; // total amount of time in the calendar
+                    double categoryTime = 0; // total amount of tine in a category
+                    for (Event e : calendarItems) {
+                        // adds up all the durations from events
+                        long start = e.getStart().getDateTime().getValue();
+                        long end = e.getEnd().getDateTime().getValue();
+                        double eventDuration = ((double) end - start) / 1000 / 60 / 60;
+                        calendarTime += eventDuration;
 
-	static {
-		try {
-			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-		} catch (Throwable t) {
-			t.printStackTrace();
-			System.exit(1);
-		}
-	}
+                        // if the event title is in one of the set categories, add it as well
+                        if (e.getSummary() != null) {
+                            for (String cat : categoryToHour.keySet()) {
+                                for (String contained : miscCategories.get(cat)) {
+                                    if (e.getSummary().contains( (CharSequence) contained)) {
+                                        categoryToHour.put(cat, categoryToHour.get(cat) + eventDuration);
+                                    }
+                                }
+                            }
+                        } eventCount += 1;
+                    } calendarToHour.put(calendarListEntry.getSummary(), calendarTime);
+                }
+            }
 
-	/**
-	 * Creates an authorized Credential object.
-	 * @return an authorized Credential object.
-	 * @throws IOException
-	 */
-	public static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in =
-			CalenTools.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets =
-			GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+            // Summarize the hours within each calendar
+            System.out.println("---- Here is Your Summary of Week " + c.dateStart + "-" + c.dateEnd + "! ----");
+            for (String calendar : calendarToHour.keySet()) {
+                double amount = calendarToHour.get(calendar);
+                String productivity = new DecimalFormat("##.##").format(amount / 168 * 100);
+                System.out.println(calendar + c.spaces(40, calendar) + amount + c.spaces(7, String.valueOf(amount)) + productivity + "%");
+            };
+            // categories, or things that are also not calendars themselves
+            for (String cat : categoryToHour.keySet()) {
+                double amount = categoryToHour.get(cat);
+                String productivity = new DecimalFormat("##.##").format(amount / 168 * 100);
+                System.out.println(cat + c.spaces(40, cat) + amount + c.spaces(7, String.valueOf(amount)) + productivity + "%");
+            };
 
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow =
-				new GoogleAuthorizationCodeFlow.Builder(
-						HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-				.setDataStoreFactory(DATA_STORE_FACTORY)
-				.setAccessType("offline")
-				.build();
-		Credential credential = new AuthorizationCodeInstalledApp(
-			flow, new LocalServerReceiver()).authorize("user");
-		// System.out.println(
-		// 		"Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-		return credential;
-	}
+            // Summarizes productivity
+            System.out.println("You were productive " + c.totalsCalendars(c.productiveAreas, calendarToHour) + "% of the time.");
 
-	/**
-	 * Build and return an authorized Calendar client service.
-	 * @return an authorized Calendar client service
-	 * @throws IOException
-	 */
-	public static com.google.api.services.calendar.Calendar
-		getCalendarService() throws IOException {
-		Credential credential = authorize();
-		return new com.google.api.services.calendar.Calendar.Builder(
-				HTTP_TRANSPORT, JSON_FACTORY, credential)
-				.setApplicationName(APPLICATION_NAME)
-				.build();
-	}
+            // Displays how long it took for the program to run
+            long endTime = System.currentTimeMillis();
+            double totalTime = (double) (endTime - startTime) / 1000.0;
+            int rate = (int) (eventCount / totalTime);
+            System.out.println("This took " + totalTime + " seconds for " + eventCount + " events. That's ~" + rate + " events per second.");
+            
+            pageToken = calendarList.getNextPageToken();
+
+        } while (pageToken != null);
+    }
+
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+        "Summary of Hours Spent From Your Calendar";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+        System.getProperty("user.home"), ".credentials/calendar-java-statistics");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart. */
+    private static final List<String> SCOPES =
+        Arrays.asList(CalendarScopes.CALENDAR_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+            CalenTools.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        // System.out.println(
+        //      "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Calendar client service.
+     * @return an authorized Calendar client service
+     * @throws IOException
+     */
+    public static com.google.api.services.calendar.Calendar
+        getCalendarService() throws IOException {
+        Credential credential = authorize();
+        return new com.google.api.services.calendar.Calendar.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
     /**
      * Adds categories for further segmentation.
      * @input  a string array of category names 
      * @input  a 2d string array of possible contained names
-     * @return treemap, a mapping of category title to possible contained names
+     * @return HashMap, a mapping of category title to possible contained names
      */
     public HashMap<String, HashSet<String>> addCategories(String[] categories, String[][] contains) {
         if (categories.length != contains.length) {
@@ -156,12 +261,11 @@ public class CalenTools {
      * @return a new string with the ideal number of spaces
      * source: http://stackoverflow.com/questions/1235179/simple-way-to-repeat-a-string-in-java
      */
-    public String generateSpaces(int numCharacters, String name) {
+    public String spaces(int numCharacters, String name) {
         return new String(new char[numCharacters-name.length()]).replace("\0", " ");
     }
 
-    // 
-    public String totalsCalendars(String[] calendars, TreeMap<String, Double> map) {
+    public String totalsCalendars(String[] calendars, HashMap<String, Double> map) {
         double sum = 0;
         for (String calendar : calendars) {
             if (map.containsKey(calendar)) {
@@ -172,103 +276,5 @@ public class CalenTools {
         return productivity;
     }
 
-    // change these variables to modify program
-    private String dateStart = "5/15/2016";
-    private String dateEnd = "5/22/2016";
-    private String wantedCalendars[] = new String[]{"Studying & Learning", "Job Searching", "Work", "Planning"};
-    private String[] categoryTitle = new String[]{"Projects", "Language"};
-    private String[][] categoryFilters = new String[][]{{"productivity", "calendar", "job manager"}, {"language", "korean", "japanese"}};
-    
-    // Instantiates a new class, CalenTools
-    public CalenTools() { }
 
-    public static void main(String[] args) throws IOException {
-        // Build a new authorized API client service. Not the calendar.model.Calendar class.
-        com.google.api.services.calendar.Calendar service =
-            getCalendarService();
-
-        // sets up the application
-        CalenTools calentools = new CalenTools();
-        TreeMap<String, Double> calendarToHour = new TreeMap<>();
-        TreeMap<String, Double> categoryToHour = new TreeMap<>();
-        HashMap<String, HashSet<String>> miscCategories; // a mapping from category name to what it contains
-        
-        DateTime now = new DateTime(System.currentTimeMillis());
-        long startTime = now.getValue();
-        int eventCount = 0;
-        miscCategories = calentools.addCategories(calentools.categoryTitle, calentools.categoryFilters);
-		
-        String pageToken = null;
-		do {
-			CalendarList calendarList = service.calendarList().list().setPageToken(pageToken).execute();
-			List<CalendarListEntry> items = calendarList.getItems();
-
-            // adds miscellaneous categories to the calendar map
-            for (String cat : miscCategories.keySet()) {
-                categoryToHour.put(cat, 0.0);
-            }
-            
-            // iterates through all the calendars
-            for (CalendarListEntry calendarListEntry : items) {
-                if (!calendarListEntry.isPrimary()) {
-                    
-                    // sets a list of events from a particular calendar
-                    Events events = service.events().list(calendarListEntry.getId())
-                        .setTimeMin(calentools.setDate(calentools.dateStart))
-                        .setTimeMax(calentools.setDate(calentools.dateEnd))
-                        .setOrderBy("startTime")
-                        .setSingleEvents(true)
-                        .execute();
-                    List<Event> calendarItems = events.getItems();
-                    
-                    double calendarTime = 0; // total amount of time in the calendar
-                    double categoryTime = 0; // total amount of tine in a category
-                    for (Event e : calendarItems) {
-                        // adds up all the durations from events
-                        long start = e.getStart().getDateTime().getValue();
-						long end = e.getEnd().getDateTime().getValue();
-						double eventDuration = ((double) end - start) / 1000 / 60 / 60;
-						calendarTime += eventDuration;
-
-                        // if the event title is in one of the set categories, add it as well
-                        if (e.getSummary() != null) {
-                            for (String cat : categoryToHour.keySet()) {
-                                for (String contained : miscCategories.get(cat)) {
-                                    if (e.getSummary().contains( (CharSequence) contained)) {
-                                        categoryToHour.put(cat, categoryToHour.get(cat) + eventDuration);
-                                    }
-                                }
-                            }
-                        } eventCount += 1;
-					} calendarToHour.put(calendarListEntry.getSummary(), calendarTime);
-				}
-			}
-
-            // Summarize the hours within each calendar
-            System.out.println("---- Here is Your Calendar Summary of Week " + calentools.dateStart + "-" + calentools.dateEnd + "! ----");
-            for (String calendar : calendarToHour.keySet()) {
-                double amount = calendarToHour.get(calendar);
-                String productivity = new DecimalFormat("##.##").format(amount / 168 * 100);
-                System.out.println(calendar + calentools.generateSpaces(44, calendar) + amount + calentools.generateSpaces(6, String.valueOf(amount)) + productivity + "%");
-            };
-            // categories, or things that are also not calendars themselves
-            for (String cat : categoryToHour.keySet()) {
-                double amount = categoryToHour.get(cat);
-                String productivity = new DecimalFormat("##.##").format(amount / 168 * 100);
-                System.out.println(cat + calentools.generateSpaces(44, cat) + amount + calentools.generateSpaces(6, String.valueOf(amount)) + productivity + "%");
-            };
-
-            // Summarizes productivity
-            System.out.println("You were productive " + calentools.totalsCalendars(calentools.wantedCalendars, calendarToHour) + "% of the time.");
-
-            // Displays how long it took for the program to run
-            long endTime = System.currentTimeMillis();
-            double totalTime = (double) (endTime - startTime) / 1000.0;
-            int rate = (int) (eventCount / totalTime);
-            System.out.println("This took " + totalTime + " seconds for " + eventCount + " events. That's ~" + rate + " events per second.");
-            
-            pageToken = calendarList.getNextPageToken();
-
-        } while (pageToken != null);
-    }
 }
